05f7e208b614784f826fe9a823585530dbe2f7dc
hemanthprudhivi27 <48275316+hemanthprudhivi27@users.noreply.github.com>
2024-01-26 16:18:19 +0530

    Update TestJenkins.java
    Added Url Config

--- TestJenkins.java
+++ TestJenkins.java
@@ -1,5 +1,5 @@
 public class TestJenkins{
       public static void main(String args[]){
-          System.out.println("Updated credential"); 
+          System.out.println("Updated new url credentials"); 
       }
 }
