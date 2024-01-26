004dcc7f480a3206a0d4f07000b89ce382193729
hemanthprudhivi27 <48275316+hemanthprudhivi27@users.noreply.github.com>
2024-01-26 15:56:35 +0530

    Update TestJenkins.java
    Updated new commit

--- TestJenkins.java
+++ TestJenkins.java
@@ -2,6 +2,6 @@ public class TestJenkins{
       public static void main(String args[]){
           System.out.println("Testing Jenkins");
           System.out.println("New commit");
-          System.out.println("Updated commit");  
+          System.out.println("Updated new  commit");  
       }
 }
