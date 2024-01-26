d819124ede37974247deedabad2a290987469def
hemanthprudhivi27 <48275316+hemanthprudhivi27@users.noreply.github.com>
2024-01-26 15:58:36 +0530

    Update TestJenkins.java
    Removed Unwanted changes

--- TestJenkins.java
+++ TestJenkins.java
@@ -1,7 +1,5 @@
 public class TestJenkins{
       public static void main(String args[]){
-          System.out.println("Testing Jenkins");
-          System.out.println("New commit");
-          System.out.println("Updated new  commit");  
+          System.out.println("Testing Jenkins"); 
       }
 }
