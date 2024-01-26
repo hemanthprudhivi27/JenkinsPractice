c1b02ce8db214a8814615bcae3f98c84b7e7ef90
hemanthprudhivi27 <48275316+hemanthprudhivi27@users.noreply.github.com>
2024-01-26 16:13:03 +0530

    Update TestJenkins.java
    Update

--- TestJenkins.java
+++ TestJenkins.java
@@ -1,5 +1,5 @@
 public class TestJenkins{
       public static void main(String args[]){
-          System.out.println("Updated credentials"); 
+          System.out.println("Updated credential"); 
       }
 }
