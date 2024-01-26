1a772bad284b1862c37b7aaf0b7ccbca7d7f081b
hemanthprudhivi27 <48275316+hemanthprudhivi27@users.noreply.github.com>
2024-01-26 16:40:47 +0530

    Update TestJenkins.java
    update url

--- TestJenkins.java
+++ TestJenkins.java
@@ -1,6 +1,6 @@
 public class TestJenkins{
       public static void main(String args[]){
 
-          System.out.println("Updated webhook urls");   
+          System.out.println("Updated webhook url");   
       }
 }
