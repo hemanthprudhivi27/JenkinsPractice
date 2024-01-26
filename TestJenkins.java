26402dd8dd379734e6671c40141e12c93057c1d4
hemanthprudhivi27 <48275316+hemanthprudhivi27@users.noreply.github.com>
2024-01-26 16:31:12 +0530

    Update TestJenkins.java
    Updated webhook

--- TestJenkins.java
+++ TestJenkins.java
@@ -1,6 +1,6 @@
 public class TestJenkins{
       public static void main(String args[]){
           System.out.println("Updated removed secrets"); 
-          System.out.println("Updated webhook url");   
+          System.out.println("Updated webhook urls");   
       }
 }
